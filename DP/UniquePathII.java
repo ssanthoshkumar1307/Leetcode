cd3eb399ce4ff0d650f8c5bfede11131434e43c3
Santhosh kumar <s.santhoshkumar1310@gmail.com>
2026-08-26 08:40:09 +0530

    Leetcode Problems

---
+++ DP/UniquePathII.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n=obstacleGrid.length;
+        int m=obstacleGrid[0].length;
+        int dp[][]=new int[n][m];
+        for(int i=0;i<m;i++)
+        {
+            if(obstacleGrid[0][i]!=1)dp[0][i]=1;
+            else break;
+        }
+        for(int j=0;j<n;j++)
+        {
+            if(obstacleGrid[j][0]!=1)dp[j][0]=1;
+            else break;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                if(obstacleGrid[i][j]==0)dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[n-1][m-1];
+        
+    }
+    
+}
