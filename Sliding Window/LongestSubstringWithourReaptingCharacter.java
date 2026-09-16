c12f15ae36917ade858a7a5504e992f8e085c3ec
Santhosh kumar <s.santhoshkumar1310@gmail.com>
2026-09-16 08:39:22 +0530

    Leetcode Problems

---
+++ Sliding Window/LongestSubstringWithourReaptingCharacter.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int max=0;
+        int left=0;
+        HashSet<Character>set=new HashSet<>();
+        for(int right=0;right<s.length();right++)
+        {
+           char ch=s.charAt(right);
+           while(set.contains(ch))
+           {
+             set.remove(s.charAt(left));
+             left++;
+           }
+           set.add(ch);
+           max=Math.max(max,right-left+1);
+        }
+      return max;   
+    }
+}
